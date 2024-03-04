2b37e1439517ce0687c906b30324eedefb7c1b48
sum_catnip <catnip@catnip.fyi>
2024-03-04 13:25:58 +0000

    fixed global coordinate system
    fixed global coords by adjusting the origin point of chunks. fast_tilemap uses the left middle as the origin while we used the center. changed by using middle left aswell

--- src/worldgen/mod.rs
+++ src/worldgen/mod.rs
@@ -19,12 +19,15 @@ impl Plugin for WorldGenPlugin {
             .add_plugins(ResourceInspectorPlugin::<WorldgenConfig>::default())
             .add_plugins(ResourceInspectorPlugin::<MapgridDebug>::default())
             .add_systems(Update, (spawn_chunks, despawn_chunks))
+            //.add_systems(Startup, spawn_test_chunk.after(setup))
             .add_systems(Update, debug_view);
     }
 }
 
 #[repr(u32)]
+#[derive(Default, Reflect)]
 pub enum Tile {
+    #[default]
     Border = 0,
     Water = 1,
     Sand = 2,
@@ -32,7 +35,7 @@ pub enum Tile {
 
 #[derive(Resource)]
 struct Tileset {
-    tex: Handle<Image>
+    tex: Handle<Image>,
 }
 
 #[derive(Resource)]
@@ -55,13 +58,15 @@ pub struct WorldgenConfig {
 #[reflect(Resource, InspectorOptions)]
 struct MapgridDebug {
     mouse_chunk: IVec2,
+    mouse_tile: IVec2,
+    mouse_tileid: Tile
 }
 
 #[derive(Default, Resource)]
 struct Chunks(HashMap<IVec2, Entity>);
 
 const TILE_SIZE: Vec2 = Vec2::new(40., 20.);
-const CHUNK_SIZE_TILES: UVec2 = UVec2::new(32, 32);
+const CHUNK_SIZE_TILES: UVec2 = UVec2::new(5, 5);
 const CHUNK_SIZE: Vec2 = Vec2::new(
     TILE_SIZE.x * CHUNK_SIZE_TILES.x as f32,
     TILE_SIZE.y * CHUNK_SIZE_TILES.y as f32,
@@ -76,13 +81,68 @@ fn setup(mut cmd: Commands, assets: Res<AssetServer>) {
 
     cmd.insert_resource(WorldgenConfig {
         render_dist: 1,
-        wavelength: 10.5,
-        land_threshhold: 0.2,
+        wavelength: 50.,
+        land_threshhold: 0.3,
     });
 
     cmd.insert_resource(TerrainNoise {
         altitude: Simplex::new(1337),
     });
+}
+
+fn spawn_test_chunk(
+    mut cmd: Commands,
+    mut mat: ResMut<Assets<Map>>,
+    ts: Res<Tileset>,
+    cfg: Res<WorldgenConfig>,
+    noise: Res<TerrainNoise>,
+) {
+    spawn_chunk(
+        &mut cmd,
+        mat.as_mut(),
+        ts.as_ref(),
+        cfg.as_ref(),
+        noise.as_ref(),
+        IVec2::ZERO,
+    );
+    spawn_chunk(
+        &mut cmd,
+        mat.as_mut(),
+        ts.as_ref(),
+        cfg.as_ref(),
+        noise.as_ref(),
+        IVec2::new(1, 1),
+    );
+    spawn_chunk(
+        &mut cmd,
+        mat.as_mut(),
+        ts.as_ref(),
+        cfg.as_ref(),
+        noise.as_ref(),
+        IVec2::new(2, 2),
+    );
+    spawn_chunk(
+        &mut cmd,
+        mat.as_mut(),
+        ts.as_ref(),
+        cfg.as_ref(),
+        noise.as_ref(),
+        IVec2::new(-1, -1),
+    );
+}
+
+fn world2grid_tile(world: Vec2) -> IVec2 {
+    let normal = world / TILE_SIZE;
+    let Vec2 { x, y } = INV_AXONOMETRIC_PROJECTION * normal;
+    IVec2 {
+        x: (x + 0.5 + 2.).floor() as i32,
+        y: (y + 0.5 + 2.).floor() as i32,
+    }
+}
+
+fn grid2world_tile(grid: IVec2) -> Vec2 {
+    // project then scale
+    TILE_SIZE * (AXONOMETRIC_PROJECTION * grid.as_vec2())
 }
 
 fn world2grid_chunk(world: Vec2) -> IVec2 {
@@ -102,6 +162,8 @@ fn grid2world_chunk(grid: IVec2) -> Vec2 {
 fn debug_view(
     window: Query<&Window>,
     cam: Query<(&Camera, &GlobalTransform)>,
+    noise: Res<TerrainNoise>,
+    cfg: Res<WorldgenConfig>,
     mut dbg: ResMut<MapgridDebug>,
 ) {
     let window = window.single();
@@ -112,7 +174,31 @@ fn debug_view(
             .map(|ray| ray.origin.truncate())
     }) {
         dbg.mouse_chunk = world2grid_chunk(worldpos);
+        dbg.mouse_tile = world2grid_tile(worldpos);
+        dbg.mouse_tileid = tile_at_pos(dbg.mouse_tile, noise.as_ref(), cfg.as_ref())
     }
+}
+
+fn spawn_chunk(
+    cmd: &mut Commands,
+    mat: &mut Assets<Map>,
+    ts: &Tileset,
+    cfg: &WorldgenConfig,
+    noise: &TerrainNoise,
+    chunkpos: IVec2,
+) -> Entity {
+    let chunk = Map::builder(CHUNK_SIZE_TILES, ts.tex.clone(), TILE_SIZE)
+        .with_projection(AXONOMETRIC)
+        .build_and_initialize(|index| init_chunk(index, chunkpos, noise, cfg));
+
+    let pos = grid2world_chunk(chunkpos);
+    cmd.spawn(MapBundleManaged::new(chunk, mat))
+        .insert(Chunk)
+        .insert(Transform {
+            translation: Vec3::new(pos.x, pos.y, 0.),
+            ..default()
+        })
+        .id()
 }
 
 fn spawn_chunks(
@@ -132,22 +218,16 @@ fn spawn_chunks(
             let vec = IVec2::new(x, y);
             if !chunks.0.contains_key(&vec) {
                 debug!("spawning chunk @ {}, curr: {}", vec, chunkpos);
-                let chunk = Map::builder(CHUNK_SIZE_TILES, ts.tex.clone(), TILE_SIZE)
-                    .with_projection(AXONOMETRIC)
-                    .build_and_initialize(|index| {
-                        init_chunk(index, vec, noise.as_ref(), cfg.as_ref())
-                    });
-
-                let pos = grid2world_chunk(vec);
                 chunks.0.insert(
                     vec,
-                    cmd.spawn(MapBundleManaged::new(chunk, mat.as_mut()))
-                        .insert(Chunk)
-                        .insert(Transform {
-                            translation: Vec3::new(pos.x, pos.y, 0.),
-                            ..default()
-                        })
-                        .id(),
+                    spawn_chunk(
+                        &mut cmd,
+                        mat.as_mut(),
+                        ts.as_ref(),
+                        cfg.as_ref(),
+                        noise.as_ref(),
+                        vec,
+                    ),
                 );
             }
         }
@@ -172,7 +252,7 @@ fn despawn_chunks(
         let chunkpos_cam = world2grid_chunk(cam_transform.translation.xy());
         let chunkpos_chunk = world2grid_chunk(chunk_transform.translation.xy());
         let dist = manhatten_dist(chunkpos_cam, chunkpos_chunk);
-        if dist > render_dist * 2 {
+        if dist > render_dist * 2 +1 {
             let e = chunks
                 .0
                 .remove(&chunkpos_chunk)
@@ -192,18 +272,13 @@ pub fn tile_at_pos(pos: IVec2, noise: &TerrainNoise, cfg: &WorldgenConfig) -> Ti
     }
 }
 
-fn init_chunk(
-    m: &mut MapIndexer,
-    chunk: IVec2,
-    noise: &TerrainNoise,
-    cfg: &WorldgenConfig,
-) {
+fn init_chunk(m: &mut MapIndexer, chunk: IVec2, noise: &TerrainNoise, cfg: &WorldgenConfig) {
     for y in 0..m.size().y {
         for x in 0..m.size().x {
-            //m.set(x, y, (((x + y) % 2) + 1) as u32);
-            let pos = IVec2::new(x as i32, y as i32);
-            let global = chunk * pos;
-            m.set(x, y, tile_at_pos(global, noise, cfg) as u32);
+            let mut pos = IVec2::new(x as i32, y as i32);
+            // global position
+            pos += chunk * CHUNK_SIZE_TILES.as_ivec2();
+            m.set(x, y, tile_at_pos(pos, noise, cfg) as u32);
         }
     }
 }
